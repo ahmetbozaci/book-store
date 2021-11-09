27fdcc3723ce6c8e218da347bc4095d17d6eead1
Ahmet <ahmt9417@gmail.com>
2021-11-09 11:35:43 +0300

    Update initialstate

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,16 +1,7 @@
-/** @format */
-import { v4 as uuidv4 } from 'uuid';
-
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [
-  {
-    id: uuidv4(),
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-  },
-];
+const initialState = [];
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
