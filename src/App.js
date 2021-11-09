f87bdb37a127ec3a4590bd48e6ae3c796462da04
Ahmet <ahmt9417@gmail.com>
2021-11-09 13:33:14 +0300

    Update navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 /** @format */
 
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/Navbar';
 import store from './redux/configureStore';
+import Books from './components/book/Books';
+import Categories from './components/Categories';
 
 const App = () => (
   <Provider store={store}>
-    <Navbar />
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Books />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
   </Provider>
 );
 
