ca717868e8518d553117c89b9cefeb78643d2f97
Ahmet <ahmt9417@gmail.com>
2021-11-12 14:38:50 +0300

    Style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import { NavLink } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa';
 
 const Navbar = () => (
-  <nav className="navbar">
-    <ul>
-      <li>
+  <nav className="navbar container">
+    <ul className="container">
+      <li className="navbar-brand">
         Bookstore CMS
       </li>
-      <li>
+      <li className="navbar-link">
         <NavLink to="/">Books</NavLink>
       </li>
-      <li>
+      <li className="navbar-link">
         <NavLink to="/categories">Categories</NavLink>
       </li>
-      <li><FaUserCircle size={40} /></li>
     </ul>
+    <div className="user-icon"><FaUserCircle size={20} /></div>
   </nav>
 
 );
